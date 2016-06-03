5d3ca4277b8ef07378e6c87a239a6c6c75a74f04
ycli <corrsboy@gmail.com>
2016-06-03 22:57:13 +0800

    init project

---
+++ app/app.comp.ts
@@ -0,0 +1,12 @@
+import {Component, OnInit} from '@angular/core';
+import {Routes, Router, ROUTER_DIRECTIVES} from '@angular/router';
+
+@Component({
+    selector: 'angular-admin',
+    template: '<h1>TEST</h1>',
+    styleUrls: [],
+    directives: [ROUTER_DIRECTIVES]
+})
+export class AppComp {
+    
+}
---
+++ app/main.ts
@@ -0,0 +1,7 @@
+import {bootstrap} from '@angular/platform-browser-dynamic';
+import {ROUTER_PROVIDERS} from '@angular/router';
+
+import {AppComp} from './app.comp';
+
+
+bootstrap(AppComp, [ROUTER_PROVIDERS]);
