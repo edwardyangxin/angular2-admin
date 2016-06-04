3693a999eda534ddb63688c72e2f449f32105663
ycli <corrsboy@gmail.com>
2016-06-05 01:07:46 +0800

    add dashboard component

--- app/app.comp.ts
+++ app/app.comp.ts
@@ -2,12 +2,24 @@ import {Component, OnInit} from '@angular/core';
 import {Routes, Router, ROUTER_DIRECTIVES} from '@angular/router';
 
 import {NavComp} from './nav.comp';
+import {DashboardComp} from './dashboard.comp';
+import {TablesComp} from './tables.comp';
+import {FormsComp} from './forms.comp';
+
 @Component({
     selector: 'angular-admin',
     templateUrl: 'assets/html/app.comp.html',
-    styleUrls: [],
     directives: [ROUTER_DIRECTIVES, NavComp]
 })
-export class AppComp {
+@Routes([
+    {path: '/dashboard', component: DashboardComp},
+    {path: '/tables', component: TablesComp},
+    {path: '/forms', component: FormsComp}
+])
+export class AppComp implements OnInit{
+    constructor(private router:Router){}
     
+    ngOnInit() {
+        this.router.navigate(['/dashboard']);
+    }
 }
---
+++ app/dashboard.comp.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+    templateUrl: 'assets/html/dashboard.comp.html'
+})
+export class DashboardComp {
+    
+}
